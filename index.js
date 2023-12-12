9c80b23a0c7bdca5e3b8674fad978a7141e6166b
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2023-12-12 11:07:12 +0530

    added a GET / route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (request, response) => {
+  response.send('welcome to my express app')
+})
+
 app.listen(4000, '0.0.0.0', () => {
   console.log('server started on port 4000')
 })
