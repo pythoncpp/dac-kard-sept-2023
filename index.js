9458a335ad6fcd0f9b0cbfd094caa593f1741acc
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2023-12-12 11:59:43 +0530

    added mysql

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const app = express()
 app.use((request, response, next) => {
   next()
 })
 app.use(express.json())
+const db = mysql.createConnection({ host: '', port: 3306 })
 
 app.delete('/', (request, response) => {
   response.send('this is a DELETE route')
