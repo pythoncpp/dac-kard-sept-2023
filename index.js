45888dfe63c31b36dff81339fd68ee6dabe4f2c2
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2023-12-12 11:19:45 +0530

    added POST / route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
   response.send('welcome to my express app')
 })
 
+app.post('/', (request, response) => {
+  response.send('this is a test POST route')
+})
+
 app.listen(4000, '0.0.0.0', () => {
   console.log('server started on port 4000')
 })
